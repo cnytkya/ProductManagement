7c0c4c987cf209bef785b7fc21e1b3fe551228ec
mehtap önder <mehtap.onder90@gmail.com>
2025-05-07 13:52:52 +0300

    #6  Created Roman and Siir

--- ProductManagement.Models/Entities/Siir.cs
+++ ProductManagement.Models/Entities/Siir.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ProductManagement.Models.Entities
+﻿namespace ProductManagement.Models.Entities
 {
-    class Siir
+    public class Siir
     {
+        //public int Id { get; set; }
+        //public string Yazar { get; set; }
+        //public string Baslik { get; set; }
+        //public int SayfaSayisi { get; set; }
+        public string Tur { get; set; } // Lirik, Epik, Pastoral vs.
+        
+
     }
 }
